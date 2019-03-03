083b81eea4079b2e8abb0f0c73e2dd720f487b00
AaronM <info@aamservices.uk>
2019-03-03 12:54:34 +0000

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+   module.exports = {
+     apps: [
+       {
+         name: 'sails',
+         cwd: '/usr/src/app/sails-app/',
+         script: 'app.js',
+         instances: 1,
+         autorestart: true,
+         watch: false,
+         max_memory_restart: '1G',
+         env: {
+           NODE_ENV: 'development',
+         },
+         env_production: {
+           NODE_ENV: 'production',
+         },
+       },
+     ],
+   };
